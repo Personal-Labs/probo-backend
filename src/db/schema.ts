e1b6f9c3852544e0222664b336aa9404529dc8ea
Rahul Bhardwaj <113882096+wraith2009@users.noreply.github.com>
2025-01-25 14:54:52 +0530

    chore : project initalization with drizzle and husky

---
+++ src/db/schema.ts
@@ -0,0 +1,14 @@
+import {
+  pgTable,
+  text,
+  timestamp,
+  integer,
+  uniqueIndex,
+  real,
+  varchar,
+  boolean,
+  serial,
+} from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+
+// Enums
