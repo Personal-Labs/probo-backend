c8314a7e8e1ce5a83ae1c9ddeafbb9de420c7d72
Rahul Bhardwaj <113882096+wraith2009@users.noreply.github.com>
2025-01-26 14:44:38 +0530

    chore : added required files for development as well as added app.ts for backend

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.use((req, res, next) => {
+  const error: any = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error: any, req: any, res: any) => {
+  console.error(error);
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+      status: error.status || 500,
+    },
+  });
+});
+
+export default app;
